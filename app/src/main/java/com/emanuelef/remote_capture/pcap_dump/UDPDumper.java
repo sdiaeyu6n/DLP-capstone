89421053564a45066d1e697a18d95e07d7013f88
SeungkeyMouse <ksg19980@gmail.com>
2022-11-26 15:52:05 +0900

    암호화

--- app/src/main/java/com/emanuelef/remote_capture/pcap_dump/UDPDumper.java
+++ app/src/main/java/com/emanuelef/remote_capture/pcap_dump/UDPDumper.java
@@ -2,7 +2,10 @@ package com.emanuelef.remote_capture.pcap_dump;
 
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.os.Build;
 import android.util.Log;
+
+import androidx.annotation.RequiresApi;
 
 import com.emanuelef.remote_capture.CaptureService;
 import com.emanuelef.remote_capture.Utils;
@@ -11,13 +14,25 @@ import com.emanuelef.remote_capture.activities.Database;
 import com.emanuelef.remote_capture.activities.Input_Num;
 import com.emanuelef.remote_capture.interfaces.PcapDumper;
 
+import org.apache.commons.codec.binary.Base64;
+
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetSocketAddress;
+import java.nio.charset.StandardCharsets;
+import java.security.spec.KeySpec;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Iterator;
+
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.PBEKeySpec;
+import javax.crypto.spec.SecretKeySpec;
 
 public class UDPDumper implements PcapDumper {
     public static final String TAG = "UDPDumper";
@@ -53,6 +68,7 @@ public class UDPDumper implements PcapDumper {
         mSocket.send(request);
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     public void dumpData(byte[] data) throws IOException {
         String appName = CaptureService.app__NAME;
@@ -86,16 +102,61 @@ public class UDPDumper implements PcapDumper {
             //code 수정 시작
             byte[] phone = ("My name is " + Phone + "App name is "+ appName +"dongguk").getBytes();
 
+            //암호화
+            String secretKey = "Secret";
+            String fSalt = "tJHnN5b1i6wvXMwzYMRk";
+
             ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
             outputStream.write(phone);
             outputStream.write(data);
 
-            byte[] result = outputStream.toByteArray();
-            //끝 (아래의 result를 data로 다시 바꿔줘야함)
+            String plain = outputStream.toString();
+            String cipherText="";
+            String dcrCipherText="";
+            try {
+                cipherText = encrypt(secretKey, fSalt, plain);
+                dcrCipherText = decrypt(secretKey, fSalt, cipherText);
+            } catch (Exception e) {
+                System.out.println("암호화실패");
+            }
 
+            byte[] result = cipherText.getBytes(StandardCharsets.UTF_8);
+            try {
+                System.out.println(pos+ "암호화==복호화:"+ plain.equals(dcrCipherText));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            //끝 (아래의 result를 data로 다시 바꿔줘야함)
             int rec_len = it.next();
             sendDatagram(result, pos, rec_len);
             pos += rec_len;
         }
     }
+    //Encrypt
+    public static String encrypt(String secretKey, String salt, String value) throws Exception {
+        Cipher cipher = initCipher(secretKey, salt, Cipher.ENCRYPT_MODE);
+        byte[] encrypted = cipher.doFinal(value.getBytes());
+        return Base64.encodeBase64String(encrypted);
+    }
+
+    public static String decrypt(String secretKey, String salt, String encrypted) throws Exception {
+        Cipher cipher = initCipher(secretKey, salt, Cipher.DECRYPT_MODE);
+        byte[] original = cipher.doFinal(Base64.decodeBase64(encrypted));
+        return new String(original);
+    }
+
+    private static Cipher initCipher(String secretKey, String salt, int mode) throws Exception {
+
+        SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
+
+        KeySpec spec = new PBEKeySpec(secretKey.toCharArray(), salt.getBytes(), 65536, 256);
+        SecretKey tmp = factory.generateSecret(spec);
+        SecretKeySpec skeySpec = new SecretKeySpec(tmp.getEncoded(), "AES");
+
+        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
+        cipher.init(mode, skeySpec, new IvParameterSpec(new byte[16]));
+        return cipher;
+    }
+
 }
